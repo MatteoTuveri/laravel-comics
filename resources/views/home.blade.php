e049dd02d480960c1da9bfe6dac97d7fbd6fca53
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-12-18 18:15:43 +0100

    initial scaffolding and add db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,16 @@
 
 @section('content')
 <main>
-    <h1>Home page <i class="fa-solid fa-home"></i></h1>
+    <section class="container">
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="card overflow-hidden">
+                        <img src="{{  $comic['thumb']  }}" class="card-img-top" alt="{{   $comic['title']  }}">
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 </main>
 @endsection
